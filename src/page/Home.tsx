967e88d60bb016b9aaa7fae2d7b1d5b9c92eb9b0
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-10-16 14:55:24 +0000

    Ui : add the feature function of background particles at home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,30 +1,38 @@
 import Button from "../components/Button.tsx";
+import BackgroundParticles from "../components/BackgroundParticles.tsx";
 import { getAsset } from "../utils/assets";
 
-function Home () {
-  const homePhotos = getAsset("home-photo.jpg")
-  const homeLogo = getAsset("Logo.png")
+function Home() {
+  const homePhotos = getAsset("home-photo.png");
+  const homeLogo = getAsset("Logo.png");
 
   return (
     <>
-      <div className="relative flex flex-col items-center bg-light justify-between overflow-hidden w-full h-full min-h-screen">
-        <img 
-          src={homeLogo} 
-          alt="Logo image" 
-          className="w-12 top-4 left-4 absolute" 
+      <div className="relative flex flex-col items-center justify-between overflow-hidden w-full h-full min-h-screen">
+        <img
+          src={homeLogo}
+          alt="Logo image"
+          className="w-12 top-4 left-4 absolute"
         />
         <div className="flex flex-col items-center justify-center flex-grow space-y-6 text-center px-4">
-          <img className="w-[20vh] max-h-xs" 
-            src={homePhotos} 
-            alt="Home photo" 
+          <img
+            className="w-[20vh] max-h-xs"
+            src={homePhotos}
+            alt="Home photo"
           />
-          <h1 className="font-quicksand text-4xl font-bold">TaskFlow</h1>
-          <h3 className="font-quicksand text-[2vh] max-sm:text-base">
+          <h1 className="font-quicksand text-dark text-4xl font-bold">TaskFlow</h1>
+          <h3 className="font-quicksand text-dark text-[1.7vh] max-sm:text-base">
             "Organize your task, keep your flow"
           </h3>
           <Button text="Get started" />
         </div>
-        <h3 className="font-quicksand text-dark font-light mb-4">© Copyright 2025 Taskflow</h3>
+        <h3 className="font-quicksand text-dark font-light mb-4">
+          © Copyright 2025 Taskflow
+        </h3>
+        {/** add background particle */}
+        <div className="absolute inset-0 -z-10">
+          <BackgroundParticles />
+        </div>
       </div>
     </>
   );
