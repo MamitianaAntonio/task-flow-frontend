bcffdfa55fab626cdbd4f929078808da414a5820
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-10-16 15:29:19 +0000

    feat : add routing to auth page and loading after click get started

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from './page/Home.tsx';
+import Auth from './page/Auth.tsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Auth />} />
         </Routes>
         <Toaster />
       </Router>
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,14 +1,28 @@
 import Button from "../components/Button.tsx";
 import BackgroundParticles from "../components/BackgroundParticles.tsx";
 import { getAsset } from "../utils/assets";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ClipLoader } from "react-spinners";
 
 function Home() {
   const homePhotos = getAsset("home-photo.png");
   const homeLogo = getAsset("Logo.png");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [fade, setFade] = useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    setFade(true);
+    setTimeout(() => {
+      navigate('/auth');
+    }, 300)
+  }
 
   return (
     <>
-      <div className="relative flex flex-col items-center justify-between overflow-hidden w-full h-full min-h-screen">
+      <div className="relative flex flex-col items-center justify-between overflow-hidden w-full h-full min-h-screen scroll-smooth">
         <img
           src={homeLogo}
           alt="Logo image"
@@ -20,17 +34,28 @@ function Home() {
             src={homePhotos}
             alt="Home photo"
           />
-          <h1 className="font-quicksand text-dark text-4xl font-bold">TaskFlow</h1>
+          <h1 className="font-quicksand text-dark text-4xl font-bold">
+            TaskFlow
+          </h1>
           <h3 className="font-quicksand text-dark text-[1.7vh] max-sm:text-base">
             "Organize your task, keep your flow"
           </h3>
-          <Button text="Get started" />
+          <Button
+            text={
+              loading ? <ClipLoader size={20} color="#F8FAFC" /> : "Get started"
+            }
+            onClick={handleClick}
+          />
         </div>
         <h3 className="font-quicksand text-dark font-light mb-4">
           © Copyright 2025 Taskflow
         </h3>
         {/** add background particle */}
-        <div className="absolute inset-0 -z-10">
+        <div
+          className={`transition-opacity duration-300 ease-in-out ${
+            fade ? "opacity-0" : "opacity-100"
+          } absolute inset-0 -z-10`}
+        >
           <BackgroundParticles />
         </div>
       </div>
