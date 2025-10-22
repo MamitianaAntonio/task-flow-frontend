f69890e2bd3095e9b069e97cc209713900b5f335
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-10-22 14:37:58 +0000

    feat : add post login function on services using axios

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,23 +2,59 @@ import type React from "react";
 import Button from "./Button.tsx";
 import Input from "./Input.tsx";
 import { useState } from "react";
+import { ClipLoader } from "react-spinners";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { login } from "../services/auth.ts";
 
 interface Props {
   switchForm : () => void;
 }
 
 const Login: React.FC<Props> = ({ switchForm }) => {
-  const [ email, setEmail ] = useState("");
-  const [ password, setPassword ] = useState("");
+  const [ email, setEmail ] = useState<string>("");
+  const [ password, setPassword ] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>();
+  const navigate = useNavigate();
+
+  // try to log an user using login function
+  const handleClick = async (e : React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (!email || !password) {
+      toast.error("All fields is required")
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const result = await login({ email, password });
+      if (result.success) {
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 300);
+      }
+    } catch (error) {
+      toast.error("An error occured during login");
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <>
       <div className="flex flex-col items-center justify-center space-y-4">
         <h2 className="text-3xl font-bold text-dark font-quicksand">Login</h2>
-        <form className="flex flex-col items-center justify-center space-y-4">
+        <form 
+          className="flex flex-col items-center justify-center space-y-4"
+          onSubmit={handleClick}
+        >
           <div className="flex flex-col items-center justify-center space-y-4">
             <h3 className="font-quicksand text-dark">Enter your email</h3>
-            <Input 
+            <Input
               type="email"
               placeholder="Enter your email here"
               value={email}
@@ -27,7 +63,7 @@ const Login: React.FC<Props> = ({ switchForm }) => {
           </div>
           <div className="flex flex-col items-center justify-center space-y-4">
             <h3 className="font-quicksand text-dark">Enter your password</h3>
-            <Input 
+            <Input
               type="password"
               placeholder="Enter your password here"
               value={password}
@@ -36,7 +72,15 @@ const Login: React.FC<Props> = ({ switchForm }) => {
           </div>
           <Button
             type="submit"
-            text="Login"
+            text={
+              isLoading ? (
+                <ClipLoader size={20} color="#F8FAFC"/>
+              ) : (
+                <span>
+                  Log in <FontAwesomeIcon icon={faArrowRight} />
+                </span>
+              )
+            }
             bgColor="primary"
             hoverColor="secondary"
           />
